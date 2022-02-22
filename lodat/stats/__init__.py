1504cb81cfc591e2a749d2e29d1c4d10807f8362
ChrisUnice <chrisunice@gmail.com>
2022-02-22 13:16:21 -0800

    removed bootstrap from init

--- lodat/stats/__init__.py
+++ lodat/stats/__init__.py
@@ -1,4 +1,3 @@
 from lodat.stats.outliers import fences
-from lodat.stats.bootstrap import bootstrap
 from lodat.stats.uncertainty import uncertainty
 from lodat.stats.standarderror import standarderror
