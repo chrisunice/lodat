9cc02e694c64dd66e27a96232a6752d48dfeda20
ChrisUnice <chrisunice@gmail.com>
2022-05-13 18:20:55 -0700

    updated sidebar to be smaller

--- lodat/application/components/sidebar.py
+++ lodat/application/components/sidebar.py
@@ -1,9 +1,13 @@
-from dash import html
+from dash import html, dcc
 import dash_bootstrap_components as dbc
 
 sidebar = dbc.Offcanvas(
     children=[
-        # todo add links to pages
+        dcc.Link('Home', id='home-link', href='/'),
+        html.Br(),
+        dcc.Link('Polar Plot', id='polar-link', href='/polarplot'),
+        html.Br(),
+        dcc.Link('Linear Plot', id='linear-link', href='/linearplot'),
     ],
     id="sidebar",
     title='Menu',
@@ -12,7 +16,8 @@ sidebar = dbc.Offcanvas(
     close_button=False,
     style=dict(
         display='flex',
-        marginTop='50px'
+        marginTop='50px',
+        width='200px'
     )
 )
 
