2baeba9390b01bef6074d6fefc963f9c4aa13ff3
ChrisUnice <chrisunice@gmail.com>
2022-05-27 16:19:56 -0700

    broke things out into components

--- lodat/application/pages/data_visualization.py
+++ lodat/application/pages/data_visualization.py
@@ -1,14 +1,17 @@
-from dash import html, dcc
-
+from dash import html
+from ..components.tabs import tab_bar
 
 data_vis_page = html.Div(
     children=[
-        dcc.Tabs(
-            id='data-vis-tabs',
-            children=[
-                dcc.Tab(label='Polar'),
-                dcc.Tab(label='Linear')
-            ]
+        tab_bar,
+        html.Div(
+            id='plot-contents',
+            style=dict(
+                display='flex',
+                justifyContent='center',
+                padding='5px'
+            )
         )
-    ]
+    ],
+    style=dict(width='100%')
 )
