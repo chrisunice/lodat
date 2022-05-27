d585879a4c4ea73274a593444ac4624f078b637b
ChrisUnice <chrisunice@gmail.com>
2022-05-27 16:19:27 -0700

    up importing display polar

--- lodat/application/callbacks/__init__.py
+++ lodat/application/callbacks/__init__.py
@@ -2,3 +2,4 @@ from .display_page import display_page
 from .toggle_sidebar import toggle_sidebar
 from .change_menu_icon import change_menu_icon
 from .handle_data import do_something_with_data
+from .display_polar import display_polar
