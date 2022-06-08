ff726147983dd313182ff05fc05ac73107a64c34
ChrisUnice <chrisunice@gmail.com>
2022-06-08 15:49:53 -0700

    updated to use dbc layout

--- lodat/application/pages/data_visualization.py
+++ lodat/application/pages/data_visualization.py
@@ -1,22 +1,37 @@
-from dash import html
 import dash_bootstrap_components as dbc
 
 from ..components.tabs import tab_bar
 
 data_vis_page = dbc.Row(
-    dbc.Col(
-        children=[
-            dbc.Row(
-                children=[
-                    dbc.Col(
-                        tab_bar,
-                        width=9
-                    )
-                ],
-                justify='center'
-            ),
-            dbc.Row(id='plot-contents', justify='center')
-        ],
-        align='stretch'
-    )
+    children=[
+        dbc.Col(
+            children=[
+                dbc.Row(
+                    id='row-1',
+                    children=[
+                        dbc.Col(
+                            id='tab-contents',
+                            children=[tab_bar],
+                            width=9
+                        )
+                    ],
+                    justify='center'
+                ),
+                dbc.Row(
+                    id='row-2',
+                    children=[
+                        dbc.Col(
+                            id='plot-contents'
+                        )
+                    ],
+                    # align='stretch',
+                    # style=dict(flex='1')
+                )
+            ],
+            align='stretch'
+        ),
+    ],
+    # align='center',
+    # justify='center',
+    style=dict(width='100%')
 )
