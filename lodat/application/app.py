2e0b6ec9936837aa0ce58a3641fcca6d33da3092
ChrisUnice <chrisunice@gmail.com>
2022-06-08 16:14:40 -0700

    updated to handle being offline

--- lodat/application/app.py
+++ lodat/application/app.py
@@ -1,12 +1,25 @@
 import dash
 import dash_bootstrap_components as dbc
 
+import requests
+
+# Stylesheets
+try:
+    request = requests.get("http://www.google.com", timeout=1)
+    sheets = [
+        dbc.themes.CYBORG,
+        dbc.icons.FONT_AWESOME
+    ]
+except (requests.exceptions.ConnectionError, requests.Timeout):
+    print('Offline, using offline sheets')
+    sheets = [
+        './assets/cyborg/bootstrap.min.css'
+        './assets/fontawesome/all.css'
+    ]
+
 # Application set up
 app = dash.Dash(
     name='LODAT',
-    external_stylesheets=[
-        dbc.themes.CYBORG,
-        dbc.icons.FONT_AWESOME,
-    ],
+    external_stylesheets=sheets,
     suppress_callback_exceptions=True
 )
