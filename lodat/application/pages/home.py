9c17935d3ea4cfb8ea5825ec4bd3b8b5f9e468cf
ChrisUnice <chrisunice@gmail.com>
2022-06-08 15:50:46 -0700

    updated to use dbc layout

--- lodat/application/pages/home.py
+++ lodat/application/pages/home.py
@@ -3,10 +3,18 @@ import dash_bootstrap_components as dbc
 from ..components.upload import upload
 
 
-home_page = dbc.Col(
+home_page = dbc.Row(
+    id='home-contents',
     children=[
-        upload
+        dbc.Col(
+            children=[
+                upload
+            ],
+            width=6,
+            align='center'
+        )
     ],
-    width=6,
-    align='center'
+    justify='center',
+    style=dict(width='100%')
 )
+
