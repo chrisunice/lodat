fe09fecf15de1a0d53beb1e85e7b45a7816c2b19
ChrisUnice <chrisunice@gmail.com>
2022-06-20 17:14:57 -0700

    updated to serve layout each time for unique session ids

--- lodat/application/index.py
+++ lodat/application/index.py
@@ -1,7 +1,7 @@
 from lodat.application.app import app
-from lodat.application.layout import layout
+from lodat.application.layout import serve_layout
 from lodat.application.callbacks import *
 
 if __name__ == "__main__":
-    app.layout = layout
+    app.layout = serve_layout
     app.run_server(debug=True, port=8888)
