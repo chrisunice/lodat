64c94dfa6d8bfdb59046050c3d4d60f49569cdc8
ChrisUnice <chrisunice@gmail.com>
2022-06-20 17:47:25 -0700

    changed layout to a callable function

--- lodat/application/layout.py
+++ lodat/application/layout.py
@@ -3,26 +3,30 @@ from dash import html, dcc
 from .components.navbar import navbar
 from .components.sidebar import sidebar
 
-app_styles = {
-    'display': 'flex',
-    'flex-direction': 'column',
-    'min-height': '100vh'
-}
 
-container_styles = {
-    'display': 'flex',
-    'flex': '1',
-}
+def serve_layout():
 
-layout = html.Div(
-    id='dash-layout',
-    children=[
-        dcc.Location(id='url', refresh=True),
-        # dcc.Store(id='session-store', storage_type='session'),
-        dcc.Loading(dcc.Store(id="cache-store"), fullscreen=True, type="dot"),  # todo still work to be done here
-        navbar,
-        sidebar,
-        html.Div(id='page-container', style=container_styles)
-    ],
-    style=app_styles
-)
+    app_styles = {
+        'display': 'flex',
+        'flex-direction': 'column',
+        'min-height': '100vh'
+    }
+
+    container_styles = {
+        'display': 'flex',
+        'flex': '1',
+    }
+    return html.Div(
+        id='dash-layout',
+        children=[
+            dcc.Location(id='url', refresh=True),
+            dcc.Store(
+                id='session-store',
+                storage_type='session',
+            ),
+            navbar,
+            sidebar,
+            html.Div(id='page-container', style=container_styles)
+        ],
+        style=app_styles
+    )
