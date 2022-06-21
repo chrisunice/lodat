0684a13625cf383bfbc6260e571d90352f6faa74
ChrisUnice <chrisunice@gmail.com>
2022-06-20 17:46:55 -0700

    callback for dash uploader

--- lodat/application/callbacks/store_data.py
+++ lodat/application/callbacks/store_data.py
@@ -1,71 +1,21 @@
-# TODO still a lot of work to be done here
-# NOTE dash-resumable-upload may be worth exploring
-import io
+import os
 import json
-import base64
-import pandas as pd
-from dash.exceptions import PreventUpdate
-# from dash.dependencies import Input, Output, State
-from dash_extensions.enrich import ServersideOutput, Input, Output, State
-
-from ..app import app
-
-import time
-import plotly.express as px
+import dash_uploader
+from dash.dependencies import Output
 
 
-@app.callback(
-    ServersideOutput('cache-store', 'data'),
-    Input('upload-data', 'contents'),
-    State('upload-data', 'filename'),
-    prevent_initial_call=True
+@dash_uploader.callback(
+    output=[Output('upload-data-output', 'children'), Output('session-store', 'data')],
+    id='upload-data',
 )
-def store_uploaded_data(content_list, name_list):
-    if content_list is None:
-        raise PreventUpdate
-
-    # Step through all the data loaded
-    for content, name in zip(content_list, name_list):
-
-        # Decode the data
-        content_type, content_string = content.split(',')
-        decoded = base64.b64decode(content_string)
-
-        # Dandle csv data
-        if name.endswith('.csv'):
-            df = pd.read_csv(io.BytesIO(decoded), index_col=0)
-
-    return df
-
-# @app.callback(
-#     [ServersideOutput('cache-store', 'data'),
-#      Output('loading-state', 'children')],
-#     Input('upload-data', 'contents'),
-#     State('upload-data', 'filename'),
-#     State('cache-store', 'data'),
-#     prevent_initial_call=True
-# )
-# def store_uploaded_data(content_list, name_list, existing_data):
-#     if content_list is None:
-#         raise PreventUpdate
-#     else:
-#         # Check to see if data already exists in the session
-#         if existing_data is not None:
-#             data = json.loads(existing_data)
-#         else:
-#             data = dict()
-#
-#         # Step through all the data loaded
-#         for content, name in zip(content_list, name_list):
-#
-#             # Decode the data
-#             content_type, content_string = content.split(',')
-#             decoded = base64.b64decode(content_string)
-#
-#             # Dandle csv data
-#             if name.endswith('.csv'):
-#                 df = pd.read_csv(io.BytesIO(decoded), index_col=0)
-#                 data[name] = df.to_dict()
-#
-#         # Convert dictionary to JSON string
-#         return json.dumps(data), ""
+def store_uploaded_data(status):
+    """
+    Stores the data from the dash uploader component and updates
+    the session store with a session unique ID and file names
+    """
+    data = {
+        'session-id': status.upload_id,
+        'files': list(map(lambda x: str(os.path.basename(x)), status.uploaded_files))
+    }
+    json_string = json.dumps(data)
+    return None, json_string
