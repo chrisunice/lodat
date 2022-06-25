2d9173fc9d5310dc1f0a27a88add281aed0b24da
ChrisUnice <chrisunice@gmail.com>
2022-06-25 15:15:05 -0700

    callback to show or hide filter icon

---
+++ lodat/application/callbacks/show_filter_icon.py
@@ -0,0 +1,13 @@
+from dash.dependencies import Output, Input, State
+
+from ..app import app
+
+
+@app.callback(Output('filter-icon', 'style'), Input('url', 'pathname'), State('filter-icon', 'style'))
+def show_filter_icon(pathname: str, style: dict):
+    on_data_vis_page = pathname == '/datavis'
+    if on_data_vis_page:
+        style['display'] = 'inline'
+    else:
+        style['display'] = 'none'
+    return style
