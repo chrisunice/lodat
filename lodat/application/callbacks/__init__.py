d9f203960f2b82c73267394ad60bdb023a3f2de9
ChrisUnice <chrisunice@gmail.com>
2022-06-25 15:15:11 -0700

    callback to show or hide filter icon

--- lodat/application/callbacks/__init__.py
+++ lodat/application/callbacks/__init__.py
@@ -3,3 +3,4 @@ from .toggle_sidebar import toggle_sidebar
 from .change_menu_icon import change_menu_icon
 from .store_data import store_uploaded_data
 from .switch_tabs import switch_tabs
+from .show_filter_icon import show_filter_icon
