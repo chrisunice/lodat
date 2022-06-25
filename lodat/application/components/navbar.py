89d6e1b556a21e91fdc52280342a5807bd6a080b
ChrisUnice <chrisunice@gmail.com>
2022-06-25 15:15:20 -0700

    added filter icon

--- lodat/application/components/navbar.py
+++ lodat/application/components/navbar.py
@@ -3,13 +3,12 @@ import dash_bootstrap_components as dbc
 
 
 navbar = dbc.Navbar(
+    id='nav-bar',
     children=[
         html.I(
             id='menu-icon',
             className='fa-solid fa-bars fa-2xl',
-            style=dict(
-                color='white'
-            )
+            style=dict(color='white')
         ),
         html.Div(
             html.H5(
@@ -17,6 +16,11 @@ navbar = dbc.Navbar(
                 style=dict(margin='0px')
             ),
             style=dict(width='100%', textAlign='center')
+        ),
+        html.I(
+            id='filter-icon',
+            className='fa-solid fa-filter fa-2xl',
+            style={'color': 'white', 'display': 'none'}
         )
     ],
     style=dict(
