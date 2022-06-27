26350f4025d6eb13c33c1f13cfa4b061ba13ca52
ChrisUnice <chrisunice@gmail.com>
2022-06-27 07:08:27 -0700

    fixed the switching of tabs

--- lodat/application/callbacks/switch_tabs.py
+++ lodat/application/callbacks/switch_tabs.py
@@ -1,4 +1,3 @@
-from dash import callback_context
 from dash.dependencies import Input, Output
 
 from ..app import app
@@ -7,25 +6,12 @@ from ..components.plots import polar_plot, linear_plot
 
 @app.callback(
     Output('row-2', 'children'),
-    [
-        Input('data-vis-tabs', 'active_tab'),
-        Input('url', 'pathname')
-    ]
+    Input('data-vis-tabs', 'active_tab')
 )
-def switch_tabs(active_tab, pathname):
-    input_fired = callback_context.triggered_id
-
-    print(input_fired, pathname)
-    if input_fired == 'url':
-        if pathname == '/datavis':
-            return polar_plot
-
-    if input_fired == 'data-vis-tabs':
-        if active_tab == 'polar':
-            return polar_plot
-        elif active_tab == 'linear':
-            return linear_plot
-        else:
-            return None
-
-# TODO need to figure out how to an active tab to show a plot on page load. pathname isnt working for /datavis
+def switch_tabs(active_tab):
+    if active_tab == 'polar':
+        return polar_plot
+    elif active_tab == 'linear':
+        return linear_plot
+    else:
+        return None
