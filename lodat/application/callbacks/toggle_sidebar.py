1d15f44ce5d7aea59a9253497b01861a5899e450
ChrisUnice <chrisunice@gmail.com>
2022-06-27 11:09:41 -0700

    formatting change

--- lodat/application/callbacks/toggle_sidebar.py
+++ lodat/application/callbacks/toggle_sidebar.py
@@ -3,10 +3,14 @@ from dash.dependencies import Input, Output, State
 from ..app import app
 
 
-@app.callback(Output("sidebar", "is_open"),
-              [Input("menu-icon", "n_clicks"),
-               Input("url", "pathname")],
-              State("sidebar", "is_open"))
+@app.callback(
+    Output("sidebar", "is_open"),
+    [
+        Input("menu-icon", "n_clicks"),
+        Input("url", "pathname")
+    ],
+    State("sidebar", "is_open")
+)
 def toggle_sidebar(menu_click, link_click, is_open):
     # If menu button is clicked open or close the sidebar
     if menu_click:
