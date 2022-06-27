e7f5ed5f037801979c6d4303a6415e751cc95e21
ChrisUnice <chrisunice@gmail.com>
2022-06-27 13:36:32 -0700

    added functionality to populate data sources

--- lodat/application/callbacks/populate_data_selector.py
+++ lodat/application/callbacks/populate_data_selector.py
@@ -13,10 +13,12 @@ checklist_style = {
 @app.callback(
     Output('data-selector-source', 'children'),
     Input('filter-icon', 'n_clicks'),
-    State('session-store', 'data')
+    State('session-store', 'data'),
+    State('data-selector-source', 'children')
 )
-def data_source(filter_click, data):
-    if filter_click:
+def data_source(filter_click, data, original_content):
+    databar_is_open = (filter_click % 2 != 0)
+    if databar_is_open and data is not None:
         data = json.loads(data)
         files = [os.path.splitext(file)[0] for file in data.get('files')]
         return dcc.Checklist(
@@ -24,3 +26,5 @@ def data_source(filter_click, data):
             inline=False,
             style=checklist_style
         )
+    else:
+        return original_content
