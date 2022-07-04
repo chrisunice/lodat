0d295654a4e570b2ae38b651a8bf3053fcbcab02
ChrisUnice <chrisunice@gmail.com>
2022-07-03 17:47:28 -0700

    updated sidebar to have icons

--- lodat/application/components/sidebar.py
+++ lodat/application/components/sidebar.py
@@ -2,30 +2,66 @@ from dash import html, dcc
 import dash_bootstrap_components as dbc
 
 
+def horizontal_line():
+    style = dict(
+        width='100%',
+        height='1px',
+        color='white'
+    )
+    return html.Hr(style=style)
+
+
+def menu_item(id_name, icon_name, text):
+    button_style = dict(display='flex', alignItems='center')
+    icon_style = dict(color='white')
+    text_style = dict(color='white', marginLeft='10px', fontSize='1rem')
+    return html.Div(
+        dbc.Button(
+            id=id_name,
+            children=[
+                html.I(className=f"fa-solid fa-{icon_name} fa-xl", style=icon_style),
+                html.Span(text, style=text_style)
+            ],
+            color='link',
+            className='text-decoration-none',
+            style=button_style
+        ),
+        style=dict(display='flex')
+    )
+
+
 sidebar = dbc.Offcanvas(
     children=[
         html.Div(
             children=[
-                dcc.Link('Home', id='home-link', href='/'),
-                html.Br(),
-                dcc.Link('Data Visualization', id='data-vis-link', href='/datavis'),
+                html.H5('Menu'),
+                horizontal_line(),
+                menu_item('menu-upload', 'upload', 'Upload'),
+                horizontal_line(),
+                dcc.Link(
+                    menu_item('menu-data-vis', 'chart-area', 'Data Visualization'),
+                    id='data-vis-link',
+                    href='/datavis',
+                    className='text-decoration-none'
+                ),
+                horizontal_line()
             ],
             style={
                 'display': 'flex',
                 'flex-direction': 'column',
-                'align-items': 'center'
+                'align-items': 'center',
             }
         )
     ],
     id="sidebar",
-    title='Menu',
     scrollable=True,
     is_open=False,
     close_button=False,
     style=dict(
         display='flex',
         marginTop='50px',
-        width='200px'
+        width='200px',
+        padding='0px',
     )
 )
 
