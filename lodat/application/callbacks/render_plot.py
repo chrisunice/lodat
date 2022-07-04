bdd96bee5fce7c5e0bf3e39a4dea2386bd07ed32
ChrisUnice <chrisunice@gmail.com>
2022-07-03 17:47:10 -0700

    trying to figure out how render plot efficiently

--- lodat/application/callbacks/render_plot.py
+++ lodat/application/callbacks/render_plot.py
@@ -1,3 +1,5 @@
+import numpy as np
+import plotly.graph_objects as go
 from dash.exceptions import PreventUpdate
 from dash.dependencies import Input, Output, State
 
@@ -8,14 +10,25 @@ from lodat.domain.data import DataObject
 @app.callback(
     Output('polar-plot', 'figure'),
     Input('submit-button', 'n_clicks'),
+    State('polar-plot', 'figure'),
     State('file-checklist', 'value'),
     State('freq-checklist', 'value'),
-    State('pol-checklist', 'value')
+    State('pol-checklist', 'value'),
+    State('depr-slider', 'value')
 )
-def render_plot(submit_click: int, files: list, freqs: list, pols: list):
+def render_plot(submit_click: int, figure: dict, files: list, freqs: list, pols: list, depr: list):
     if submit_click is None:
         raise PreventUpdate
 
+    fig = go.Figure(figure)
     for file, freq, pol in zip(files, freqs, pols):
         obj = DataObject(file)
-        # todo need to be able to get bin data from merge data
+        bin_data = obj.get_bin_data(freq, pol, np.mean(depr), bin_size=(1, np.abs(np.subtract(*depr))))
+        fig.add_trace(
+            go.Scatterpolargl(
+                theta=bin_data.index,
+                r=bin_data.RCS,
+                name=file
+            )
+        )
+    return fig
