5a455df0ccd277c853595c8111618be666c80a94
ChrisUnice <chrisunice@gmail.com>
2022-07-04 22:30:31 -0700

    got a basic working example of the image carousel

--- lodat/application/callbacks/imagery/display_images.py
+++ lodat/application/callbacks/imagery/display_images.py
@@ -6,13 +6,12 @@ from dash.dependencies import Input, Output
 from lodat.application.app import app
 from lodat.application import config
 
-list_of_images = [os.path.basename(x) for x in glob.glob('{}*.png'.format(config.image_folder))]
+list_of_images = [os.path.basename(path) for path in glob.glob(f"{config.image_folder}/*.png")]
 static_image_route = '/static/'
 
 
 @app.server.route('{}<image_path>.png'.format(static_image_route))
 def serve_images(image_path):
-    print('hello world')
     image_name = f"{image_path}.png"
     if image_name not in list_of_images:
         raise Exception('"{}" is excluded from the allowed static files'.format(image_path))
@@ -21,7 +20,7 @@ def serve_images(image_path):
 
 @app.callback(
     Output('imagery-carousel', 'items'),
-    Input('test-button', 'n_clicks')
+    Input('database-icon', 'n_clicks')
 )
 def display_images(click):
     if click:
@@ -29,7 +28,7 @@ def display_images(click):
         for idx, path in enumerate(list_of_images):
             items.append(
                 dict(
-                    key=str(idx),
+                    key=str(idx+1),
                     src=f'{static_image_route}{path}',
                     caption=f'{os.path.splitext(path)[0]}'
                 )
--- lodat/application/pages/imagery.py
+++ lodat/application/pages/imagery.py
@@ -1,15 +1,38 @@
-"""
-TODO fix the carousel that came out at a stupid small size when given images
-"""
 from dash import html
 import dash_bootstrap_components as dbc
+
+page_style = {
+    'display': 'flex',
+    'flex-direction': 'column',
+    'width': '100%',
+    'justify-content': 'center',
+    'align-items': 'center'
+}
+
+
+image_style = {
+    'max-height': '480px',
+    'max-width': '640px',
+    'box-shadow': '0px 0px 10px #fff'
+}
 
 imagery_page = html.Div(
     id='imagery-page',
     children=[
-        dbc.Carousel(id='imagery-carousel', items=[], style={'max-height': '500px'}),
-        html.Img(id='test-image', style={'max-height': '50px'}),
-        dbc.Button("Test", id='test-button', color='primary')
-    ]
+        dbc.Carousel(
+            id='imagery-carousel',
+            items=[
+                {
+                    'key': '1',
+                    'src': '../static/black_image.jpg',
+                    'header': 'This is my header',
+                    'caption': 'This is my caption'
+                }
+            ],
+            ride=False,
+            style=image_style
+        )
+    ],
+    style=page_style
 )
 
