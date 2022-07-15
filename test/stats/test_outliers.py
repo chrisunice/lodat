8061856479ea062c55d6603b9a889b8641537bc5
ChrisUnice <chrisunice@gmail.com>
2022-07-14 18:57:40 -0700

    fixed imports and tests

--- test/stats/test_outliers.py
+++ test/stats/test_outliers.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import pytest
 import numpy as np
-from lodat.stats.outliers import fences
+
+import lodat as lo
 
 
 @pytest.fixture
@@ -13,28 +14,28 @@ def test_fences(my_list):
     my_outlier = 10000
     my_list.append(my_outlier)
     arr = np.array(my_list)
-    interval = fences(arr, coverage_factor=1.5)
+    interval = lo.stats.fences(arr, coverage_factor=1.5)
     assert max(interval) < my_outlier
 
 
 def test_fences_dims():
     arr_2d = np.random.random(size=(10, 10))
     with pytest.raises(AssertionError):
-        fences(arr_2d)
+        lo.stats.fences(arr_2d)
 
 
 def test_list_type(my_list):
-    interval = fences(my_list)
+    interval = lo.stats.fences(my_list)
     assert isinstance(interval, tuple)
 
 
 def test_array_type(my_list):
     my_arr = np.array(my_list)
-    interval = fences(my_arr)
+    interval = lo.stats.fences(my_arr)
     assert isinstance(interval, tuple)
 
 
 def test_series_type(my_list):
     my_series = pd.Series(my_list)
-    interval = fences(my_series)
+    interval = lo.stats.fences(my_series)
     assert isinstance(interval, tuple)
