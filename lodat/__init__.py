36c064d91b2781165618ac7760aab7c972a4f979
ChrisUnice <chrisunice@gmail.com>
2022-07-14 18:51:55 -0700

    fixed imports and tests

--- lodat/__init__.py
+++ lodat/__init__.py
@@ -6,3 +6,8 @@ class Configuration:
     def __init__(self):
         self.config = ConfigParser()
         self.config.read(f"{os.path.dirname(__file__)}\\config.ini")
+
+from .domain.data import DataObject
+from .domain.analysis import Algo
+from .domain import stats
+from .domain import utils
--- test/data/test_data_object.py
+++ test/data/test_data_object.py
@@ -1,13 +1,13 @@
-from lodat.data import DataObject
+import lodat as lo
 
 
 def test_raw_data(test_assets_path):
-    obj = DataObject(f"{test_assets_path}\\test_data.csv")
+    obj = lo.DataObject(f"{test_assets_path}\\test_data.csv")
     assert not obj.raw_data.empty
 
 
 def test_data(test_assets_path):
-    obj = DataObject(f"{test_assets_path}\\test_data.csv")
+    obj = lo.DataObject(f"{test_assets_path}\\test_data.csv")
     assert bool(obj.data)
 
 
