94b3959287bdd5d9e2d1a98ff9743266e09c6b07
ChrisUnice <chrisunice@gmail.com>
2022-07-14 18:50:29 -0700

    fixed imports and tests

--- test/analysis/test_algo.py
+++ test/analysis/test_algo.py
@@ -1,28 +1,27 @@
 import pytest
+import lodat as lo
 import pandas as pd
-from lodat.analysis import Algo
-from lodat.data import DataObject
 
 
 @pytest.fixture
 def data_objects(test_assets_path):
-    test = DataObject(f"{test_assets_path}\\test_data.csv")
-    base = DataObject(f"{test_assets_path}\\baseline_data.csv")
+    test = lo.DataObject(f"{test_assets_path}\\test_data.csv")
+    base = lo.DataObject(f"{test_assets_path}\\baseline_data.csv")
     return [test, base]
 
 
 def test_load(data_objects):
-    algo = Algo(*data_objects)
-    assert isinstance(algo, Algo)
+    algo = lo.Algo(*data_objects)
+    assert isinstance(algo, lo.Algo)
 
 
 def test_analyze_normal(data_objects):
-    algo = Algo(*data_objects)
+    algo = lo.Algo(*data_objects)
     results = algo.analyze(10000, 'VV', bootstrap=False)
     assert isinstance(results, pd.DataFrame)
 
 
 def test_analyze_strapped(data_objects):
-    algo = Algo(*data_objects)
+    algo = lo.Algo(*data_objects)
     results = algo.analyze(10000, 'VV', bootstrap=True)
     assert isinstance(results, pd.DataFrame)
