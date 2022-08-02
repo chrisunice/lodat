de24cee7a1d18075c8f0b0aacae46dbe0cb530ea
ChrisUnice <chrisunice@gmail.com>
2022-08-01 18:08:53 -0700

    figured out how to host dash app on LAN

--- lodat/application/index.py
+++ lodat/application/index.py
@@ -5,4 +5,4 @@ from lodat.application.callbacks import *
 
 if __name__ == "__main__":
     app.layout = serve_layout
-    app.run_server(debug=True, port=8888)
+    app.run_server(debug=False, host="0.0.0.0", port=8050)
